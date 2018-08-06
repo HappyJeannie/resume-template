ffce7fe1af368a77ac5edce4fa45e61df4067c81
HappyJeannie <436030684@qq.com>
2018-08-06 10:25:40 +0800

    【增加】- 登录/注册弹窗

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@ let vm = new Vue({
   el : '#app',
   data(){
     return {
-      isediting:false,
+      showLogin:false,
+      showRegist:false,
       resume : {
         name : 'Summer',
         age : '22',
@@ -15,6 +16,29 @@ let vm = new Vue({
   methods:{
     editProfile(val,event){
       this.resume[val] = event;
+    },
+    clickSaveBtn(){
+      var currentUser = AV.User.current();
+      if (currentUser) {
+        this.save();
+      }
+      else {
+        this.toLogin();
+      }
+    },
+    save(){
+      console.log('保存用户信息')
+      let User = AV.Object.extend('User');
+      let user = new User();
+      user.set('resume',this.resume);
+      user.save().then(function (user) {
+        console.log(user);
+      }, function (error) {
+        console.error(error);
+      });
+    },
+    toLogin(){
+      console.log('跳转登录')
     }
   }
 })
